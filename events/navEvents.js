d0a94876e78aaaf1698a504533fa81d93c3a70f1
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2024-12-13 23:12:18 -0600

    user can search revenue by date, minor other fixes

--- events/navEvents.js
+++ events/navEvents.js
@@ -6,7 +6,7 @@ import { getRevenue } from "../api/apiRevenue";
 import createOrderForm from "../Form/createOrderForm";
 
 const navEvent = (user) => {
-    document.querySelector('#NavivationRefs').addEventListener('click', (e) => {
+    document.querySelector('#NavigationRefs').addEventListener('click', (e) => {
         e.preventDefault();
 
         document.querySelector('#create-order').addEventListener('click', () => {
