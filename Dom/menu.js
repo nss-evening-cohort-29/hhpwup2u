9a1b5e102b920f3bf98d01e9c54161814eb77bc1
Damitry Dong <Damitry111@gmail.com>
2024-12-17 17:42:33 -0600

    yay

--- Dom/menu.js
+++ Dom/menu.js
@@ -8,23 +8,28 @@ const showMenuItems = (array, admin) => {
   let domString = ""
 
   if (admin === 2) {
-    domString += '<button type="button" class="btn btn-success" id="add-menu-btn">Add Item</button>';
+    domString += '<button type="button" class="menu-btn" id="add-menu-btn">Add Item</button>';
   }
 
+  domString += `<div class="mmmm" id="menu-card-container-container">`
   array.forEach((item) => {
     domString += `
-    <div class="menu-item-card" style="width: 15rem;">
-    <div class="menu-item-body">
-      <h5 class="menu-item-title">${item.menuItemName}</h5>
-      <hr>
+    <div class="menu-card-container">
+      <div class="menu-item-card">
+        <div class="menu-item-body">
+        <h1 class="menu-item-title">${item.menuItemName}</h1>
+        <hr>
+        </div>
+        <img class="menu-card-img-top" src=${item.menuItemImage} alt=${item.menuItemName} ">
+        <h2 class="card-text bold">${item.menuItemSale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i>💸  </span> $${item.menuItemPrice}` : `$${item.menuItemPrice}`}</h2>
+        ${admin === 2 ? `<a href="#" class="card-link" id="delete-menu-btn--${item.firebaseKey}">Delete</a>` : ''}
+        ${admin === 2  ? `<a href="#" class="card-link" id="edit-menu-btn--${item.firebaseKey}">Edit</a>` : ''}
+        <button type="button" class="menu-order-btn" id="Order-menu-btn--${item.firebaseKey}">Order</button>
       </div>
-      <img class="menu-card-img-top" src=${item.menuItemImage} alt=${item.menuItemName} style="height: 225px;">
-      <p class="card-text bold">${item.menuItemSale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.menuItemPrice}` : `$${item.menuItemPrice}`}</p>
-      ${admin === 2 ? `<a href="#" class="card-link" id="delete-menu-btn--${item.firebaseKey}">Delete</a>` : ''}
-      ${admin === 2  ? `<a href="#" class="card-link" id="edit-menu-btn--${item.firebaseKey}">Edit</a>` : ''}
-      <button type="button" class="btn btn-success" id="Order-menu-btn--${item.firebaseKey}">Order</button>
-  </div>`;
+    </div>`;
   });
+  domString += `</div>`
+
   renderToDOM('#main-container', domString);
 };
 
